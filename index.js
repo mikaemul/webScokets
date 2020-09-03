ff4468757f5a91c0e52f006f23060814df72c8d6
mikaemul <mikael.multanen@metropolia.fi>
2020-09-03 20:26:52 +0300

    websockets0309

--- index.js
+++ index.js
@@ -1,24 +1,72 @@
 'use strict';
 
 const express = require('express');
+const { disconnect } = require('process');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const formatMessage = require('./utils/messages');
+const {userJoin,getCurrentUser,getRoomUsers,userLeave } = require('./utils/users');
 
 app.use(express.static('public'));
 
-io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
+const botName = 'ChatBot';
 
-  socket.on('disconnect', () => {
-    console.log('a user disconnected', socket.id);
+io.on('connection', (socket) =>{
+  socket.on('joinRoom', ({username,room}) =>{
+    const user = userJoin(socket.id,username,room);
+
+    socket.join(user.room);
+
+     //welcome user to chat
+  socket.emit('message',formatMessage(botName,'Welcome to the chat!'));
+
+  //Broadcast when user connects
+  socket.broadcast
+    .to(user.room)
+    .emit(
+      'message',
+      formatMessage(botName, `${user.username} has joined the chat.`)
+      );
+
+      //send users and room info
+      io.to(user.room).emit('roomUsers',{
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
   });
 
-  socket.on('chat message', (msg) => {
-    console.log('message: ', msg);
-    io.emit('chat message', msg);
+  //lisen for chat message
+  socket.on('chat message', (msg)=>{
+
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit('message',formatMessage(user.username,msg));
   });
+
+
+  //when user disconnects
+  socket.on('disconnect', ()=>{
+
+    const user = userLeave(socket.id);
+
+    if(user){
+      io.to(user.room).emit('message',formatMessage(botName, `${user.username} has left the chat.`));
+      
+      //send users and room info
+      io.to(user.room).emit('roomUsers',{
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    }
+
+
+   
+  });
+
 });
+
+
+
 
 http.listen(3000, () => {
   console.log('listening on port 3000');
--- public/chat.js
+++ public/chat.js
@@ -1,16 +1,73 @@
 'use strict';
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages'); 
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 
-const socket = io();
+// get username and room from url
+const {username,room} = Qs.parse(location.search,{
+  ignoreQueryPrefix: true
+});
+console.log(username, room);
+const socket = io('http://10.114.32.17');
 
-document.querySelector('form').addEventListener('submit', (event) => {
+//join chat room
+socket.emit('joinRoom', {username, room});
+
+// get users and room
+socket.on('roomUsers',({room, users})=>{
+  outputRoomName(room);
+  outputUsers(users);
+});
+//message from server
+socket.on('message', (message)=>{
+  console.log(message);
+  outputMessage(message);
+
+  //scroll down 
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+
+});
+
+// message submit
+chatForm.addEventListener('submit', (event)=>{
   event.preventDefault();
-  const inp = document.getElementById('m');
-  socket.emit('chat message', inp.value);
-  inp.value = '';
+
+  //get message
+  const msg = event.target.elements.msg.value;
+
+  //emit message to server
+  socket.emit('chat message', msg);
+
+  //clear input
+  event.target.elements.msg.value = '';
+  event.target.elements.msg.focus();
 });
 
-socket.on('chat message', (msg) => {
-  const item = document.createElement('li');
-  item.innerHTML = msg;
-  document.getElementById('messages').appendChild(item);
-});
+//output message to dom
+function outputMessage(message){
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `<p class="meta">${message.username}</p>
+  <p class="text">
+    ${message.text}
+  </p>`;
+  document.querySelector('.chat-messages').appendChild(div);
+};
+
+//add room name to dom 
+function outputRoomName(room){
+  roomName.innerText = room;
+};
+
+//add users to dom 
+function outputUsers(users){
+  userList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join('')}
+  `;
+};
+
+
+
+
+
